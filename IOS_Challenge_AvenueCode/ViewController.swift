94ff17ff696c99d1a8aa24ec1c2afe1d5ad4cce1
henriquemeirelesvaladares <henriquemv2003@yahoo.com.br>
2018-06-13 18:10:28 -0300

    Initial Commit

---
+++ IOS_Challenge_AvenueCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS_Challenge_AvenueCode
+//
+//  Created by Henrique Valadares on 13/06/18.
+//  Copyright © 2018 Henrique Valadares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
